48617d15f2632cb86b9a4bbb59c6f924a9e89d46
Anderson Navarro <andersonaugustonb@ufps.edu.co>
2024-04-17 22:38:51 -0500

    FEATURE: Creacion y acople del servicio

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   template: `
     <h5>Buscar:</h5>
-    <input type="text" class="form-control" placeholder="Buscar gifs..." />
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Buscar gifs..."
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+    />
   `,
 })
+
+// #txtTagInput es una referencia local para nuestro evento keyup
+// y asi evitamos utilizar el formsModule para algo mas sencillo
 export class SearchBoxComponent {
+  // ! significa que el elemento no sera nulo
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
   constructor() {}
+
+  // searchTag(newTag: string) {
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({ newTag });
+  }
 }
